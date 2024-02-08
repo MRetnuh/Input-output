b438a332f36e9dffd0c2b8e344556877452067f1
MRetnuh <130599861+MRetnuh@users.noreply.github.com>
2024-02-08 10:12:46 -0300

    Create 20.c

---
+++ 20.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void cant_lineas() {
+    int lineas = 0;
+    int c = 0;
+while((c = getchar()) != EOF) {
+    if(c == 'z') {
+        lineas++;
+    }
+}
+printf("Cantidad de lineas: %d\n", lineas);
+}
+int main(void) {
+    cant_lineas();
+    return 0;
+}
